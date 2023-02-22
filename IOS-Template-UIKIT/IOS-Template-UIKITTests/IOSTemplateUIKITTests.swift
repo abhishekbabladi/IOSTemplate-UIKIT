d85fd4c8d0d50daf557834972705c7676a8c5c3e
Abhishek Babladi <abhishek.babladi@netlight.com>
2023-02-22 15:53:33 +0100

    Updated

--- IOS-Template-UIKIT/IOS-Template-UIKITTests/IOSTemplateUIKITTests.swift
+++ IOS-Template-UIKIT/IOS-Template-UIKITTests/IOSTemplateUIKITTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import IOS_Template_UIKIT
 
-final class IOS_Template_UIKITTests: XCTestCase {
+final class IOSTemplateUIKITTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
