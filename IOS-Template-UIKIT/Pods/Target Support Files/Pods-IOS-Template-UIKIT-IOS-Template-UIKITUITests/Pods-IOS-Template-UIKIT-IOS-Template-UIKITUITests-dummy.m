5c055e2d7967f65fd7120872d35e17f1edbf36b3
Abhishek Babladi <abhishek.babladi@netlight.com>
2023-02-22 15:38:34 +0100

    SwiftLint added

---
+++ IOS-Template-UIKIT/Pods/Target Support Files/Pods-IOS-Template-UIKIT-IOS-Template-UIKITUITests/Pods-IOS-Template-UIKIT-IOS-Template-UIKITUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOS_Template_UIKIT_IOS_Template_UIKITUITests : NSObject
+@end
+@implementation PodsDummy_Pods_IOS_Template_UIKIT_IOS_Template_UIKITUITests
+@end
---
+++ IOS-Template-UIKIT/Pods/Target Support Files/Pods-IOS-Template-UIKIT/Pods-IOS-Template-UIKIT-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOS_Template_UIKIT : NSObject
+@end
+@implementation PodsDummy_Pods_IOS_Template_UIKIT
+@end
